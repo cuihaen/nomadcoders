6c9a77e6d5bae56e967f7e9dc0cd034e04fcd01d
cuiheen <choi_haeun367@naver.com>
2023-04-06 23:35:51 +0900

    array의 특정 요소 불러오기

--- crome_app/app.js
+++ crome_app/app.js
@@ -13,4 +13,5 @@ const dayOfWeek = [mon,tue,wed,thu,fri,sat,sun];
 //[]로 감싼 array에서는 뭐든 넣을 수 있으며, 각 요소들 사이에는 반드시 ,로 구분지어주어야 한다.
 const nonSense = [1,2,"hello",false,null,true,undefined,"haeun"];
 
-console.log(dayOfWeek,nonSense);
+//array명 뒤에 []를 추가한 후 가져오고자 하는 값의 순서를 적어주면 그 값을 불러올 수 있다.
+console.log(dayOfWeek[5]);
