0bb6aae751113b908ca9b08725cda3090b541446
cuiheen <choi_haeun367@naver.com>
2023-04-06 23:29:47 +0900

    array학습

--- crome_app/app.js
+++ crome_app/app.js
@@ -1,1 +1,13 @@
-alert('hi');
+//arrays
+
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+
+const dayOfWeek = mon+tue+wed+thu+fri+sat+sun;
+
+console.log(dayOfWeek);
