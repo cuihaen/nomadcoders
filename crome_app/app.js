53750e76d64fd918818c3a9df0c192e4007032b0
cuiheen <choi_haeun367@naver.com>
2023-04-07 00:07:57 +0900

    function

--- crome_app/app.js
+++ crome_app/app.js
@@ -1,33 +1,10 @@
-/* array로 표현했을 때 */
-// const playerName = "nico";
-// const playerPoints = 121212;
-// const playerHandsome = false;
-// const playerFat = "little bit";
+//function
 
-//player[0] = name;
-//player[1] = points;
-
-//const player = ["nico",121212,false,"little bit"];
-
-
-//object
-const player = {
-    name : "nico",
-    points : 10,
-    handSome : false,
-    fat : "little bit",
+function sayHello(){
+    console.log("hello!");
 }
 
-console.log(player);
-console.log(player.name);
-console.log(player["name"]); //<-바로 위의 것과 같은 결과값이 찍히게 된다.
-player.name; // <- object!
-
-player.handSome = true;
-player.lastName = "potato"; //새로운 object추가하기
-
-console.log(player);
-
-player.points = player.points + 5; //기존 player points에 5를 더하기!
-
-console.log(player);
+sayHello();
+sayHello();
+sayHello();
+sayHello();
