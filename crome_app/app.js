561327a5a33d528db730ba135d1fbf46a66953b0
cuiheen <choi_haeun367@naver.com>
2023-04-06 23:33:50 +0900

    add 주석

--- crome_app/app.js
+++ crome_app/app.js
@@ -10,6 +10,7 @@ const sun = "sun";
 
 const dayOfWeek = [mon,tue,wed,thu,fri,sat,sun];
 
+//[]로 감싼 array에서는 뭐든 넣을 수 있으며, 각 요소들 사이에는 반드시 ,로 구분지어주어야 한다.
 const nonSense = [1,2,"hello",false,null,true,undefined,"haeun"];
 
 console.log(dayOfWeek,nonSense);
