c27336a61b4e1e5eb6a871d4eb0d13012597335f
cuiheen <choi_haeun367@naver.com>
2023-04-11 16:09:56 +0900

    parseInt를 활용해 값을 숫자로 변환

--- crome_app/app.js
+++ crome_app/app.js
@@ -1,3 +1,5 @@
-const age = prompt("How old are you?");
+const age = parseInt(prompt("How old are you?"));
 
+//console.log(typeof "15",typeof parseInt("15"));
 console.log(age);
+//parseInt는 문자를 처리할 수 없다. = NaN
