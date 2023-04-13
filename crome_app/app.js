f5c2c6ac7973cef16cb62a9e8150c49c7457fef3
cuiheen <choi_haeun367@naver.com>
2023-04-13 17:15:55 +0900

    css에 별도 class를 만든 후 js를 활용해 class를 해당 태그에 추가

--- crome_app/app.js
+++ crome_app/app.js
@@ -1,14 +1,7 @@
 const h1 = document.querySelector('.hello h1');
 
 function handleTitleClick(){
-    const currentColor = h1.style.color;
-    let newColor;
-    if(currentColor === 'blue'){
-        newColor = 'tomato';
-    }else{
-        newColor = 'blue';
-    }
-    h1.style.color = newColor;
+    h1.className = 'active';
 }
 
 h1.addEventListener('click', handleTitleClick);
