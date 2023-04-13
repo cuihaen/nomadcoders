bbd45c999dfd638bdfbf180a9ab9c3cb02334a54
cuiheen <choi_haeun367@naver.com>
2023-04-13 17:10:35 +0900

    add 주석

--- crome_app/app.js
+++ crome_app/app.js
@@ -12,3 +12,7 @@ function handleTitleClick(){
 }
 
 h1.addEventListener('click', handleTitleClick);
+
+// step01. find the element.
+// step02. listen for an event.
+// step03. react to that event.(reaction means show something or hide something or change the color of something)
