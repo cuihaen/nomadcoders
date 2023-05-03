6c3cbfaebb02cfc1c8bc0d222ac01c2379a8023d
cuiheen <choi_haeun367@naver.com>
2023-05-04 00:24:55 +0900

    input값에 여러 조건 걸기

--- crome_app/app.js
+++ crome_app/app.js
@@ -2,7 +2,17 @@ const loginInput = document.querySelector('#login-form input');
 const loginButton = document.querySelector('#login-form button');
 
 function onLoginBtnClick(){
-    console.dir(loginInput.value);
+    const username = loginInput.value;
+    if(username === "" ){
+        alert('Please write your name.');
+    }else if(username.length >= 15 ){
+        alert('Your name is too long.');
+    }else if(username.length < 3 ){
+        alert('Your name is too short.');
+    }else{
+        console.dir(loginInput.value);
+    }
+   
     //console.log('click!');
 }
 loginButton.addEventListener('click',onLoginBtnClick);
