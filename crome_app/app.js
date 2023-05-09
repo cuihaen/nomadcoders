a3100541d02a8d4416b708e9161c90857f829279
cuiheen <choi_haeun367@naver.com>
2023-05-09 21:45:46 +0900

    class활용 username보이기

--- crome_app/app.js
+++ crome_app/app.js
@@ -1,18 +1,15 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
-const link = document.querySelector('a');
+const greeting = document.querySelector('#greeting');
+const HIDDEN_CLASSNAME = 'hidden';
 
 function onLoginSubmit(e){
     e.preventDefault();
-    console.log(loginInput.value);
-}
-
-function handleLink(e){
-    e.preventDefault();
-    alert('clicked!')
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    greeting.innerText = 'Hello '+username;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit",onLoginSubmit);
-link.addEventListener("click",handleLink);
-
 //event.preventDefault를 넣어주면 해당 브라우저가 기본동작으로 새로고침을 실행하는 것을 막아줄 수 있다.
