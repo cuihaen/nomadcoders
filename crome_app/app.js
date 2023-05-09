b72ed0f02c8f46a2e9dab858906bea9ffc38dce0
cuiheen <choi_haeun367@naver.com>
2023-05-09 22:11:03 +0900

    localStorage를 활용해 브라우저에 특정 값 저장

--- crome_app/app.js
+++ crome_app/app.js
@@ -7,10 +7,9 @@ function onLoginSubmit(e){
     e.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    greeting.innerText = 'Hello '+username;
-    // greeting.innerText = `Hello ${username}`; 위와 동일함!
+    localStorage.setItem('username',username);
+    greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit",onLoginSubmit);
-//event.preventDefault를 넣어주면 해당 브라우저가 기본동작으로 새로고침을 실행하는 것을 막아줄 수 있다.
