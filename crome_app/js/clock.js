2b9d91d01ccc5a3ca9be8d7bf8b964cb24ecb8a3
cuiheen <choi_haeun367@naver.com>
2023-05-10 17:09:34 +0900

    setTimeout vs setInterval

--- crome_app/js/clock.js
+++ crome_app/js/clock.js
@@ -4,5 +4,8 @@ function sayHello(){
     console.log('Hello');
 }
 
-setInterval(sayHello, 5000);
+//setInterval(sayHello, 5000);
 //sayHello를 5초에 한번씩 실행
+
+setTimeout(sayHello, 5000);
+//sayhello를 5초 뒤에 한번 실행
