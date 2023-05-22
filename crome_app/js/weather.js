4afd8f8d90dd890eef9953cafdca9b501151086c
cuiheen <choi_haeun367@naver.com>
2023-05-22 22:14:14 +0900

    openweathermap.org사이트에서 날씨API연결

--- crome_app/js/weather.js
+++ crome_app/js/weather.js
@@ -1,11 +1,13 @@
 // 브라우저에서 위치좌표를 알아서 잡아주게 됨.
 //getCurrentPosition(성공했을때,실패했을때) 두가지 요소가 필요함
 
+const API_KEY = "589a809e2d1edb4de7f20e997824e8d5"
 function onGeoOk(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
 
-    console.log("You live in",lat,lng);
+    console.log(url);
 }
 
 function onGeoError(){
